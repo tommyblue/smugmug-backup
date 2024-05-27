7cb31831baafcf34c2302edd29c1b69b13955143
Tommaso Visconti <tommaso.visconti@gmail.com>
2024-05-28 00:07:35 +0200

    add tailwind

--- gui/src/app.tsx
+++ gui/src/app.tsx
@@ -1,4 +1,8 @@
 import { createRoot } from "react-dom/client"
 
+const App = () => {
+	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+}
+
 const root = createRoot(document.getElementById("root"))
-root.render(<h2>Hello from React!</h2>)
+root.render(<App />)
