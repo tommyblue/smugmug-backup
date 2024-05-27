13a450c01fc2dbdcdf15794178bac6fff61b3f9e
Tommaso Visconti <tommaso.visconti@gmail.com>
2024-05-27 23:25:20 +0200

    add react

---
+++ gui/src/app.tsx
@@ -0,0 +1,4 @@
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+root.render(<h2>Hello from React!</h2>)
--- gui/src/renderer.ts
+++ gui/src/renderer.ts
@@ -26,6 +26,8 @@
  * ```
  */
 
-import './index.css';
+import "./index.css"
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+console.log('👋 This message is being logged by "renderer.ts", included via Vite')
+
+import "./app"
